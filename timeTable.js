70a9d0c3ea91e731b37ab0db562cdb1ec35f3b3d
A Jeevan Reddy <ajeevanreddy2004@gmail.com>
2025-07-02 20:41:15 +0530

    code

---
+++ timeTable.js
@@ -0,0 +1,244 @@
+const Sunday = [
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: '',
+        faculty: ''
+    }
+];
+
+const Monday = [
+    {   
+        time: '08:25 AM - 09:15 AM',
+        roomNumber: 'AB-103',
+        subject: '22AIE457 - Full Stack Development[P.E.4]',
+        type: '',
+        faculty: 'Dr.G.BharathiMohan, Ms M Rithani [Assist]'
+    },
+    {   
+        time: '09:15 AM - 10:05 AM',
+        roomNumber: 'AB-103',
+        subject: '22AIE401 Lab 2 - Reinforcement Learning Lab', // Assuming Lab for 22AIE401
+        type: '',
+        faculty: 'Dr. R.PrasannaKumar, Ms.G.Pallavi [Assist]'
+    },
+    {   
+        time: '11:10 AM - 12:00 PM',
+        roomNumber: 'AB-103',
+        subject: '19MEE347 - Total quality management[F.E.2]',
+        type: '',
+        faculty: 'Dr. Vignesh M'
+    },
+    {   
+        time: '12:00 PM - 12:50 PM',
+        roomNumber: 'AB-103',
+        subject: 'Library',
+        type: '',
+        faculty: 'AJ.Srikanth and Saptarshi chatterjee'
+    },
+    {   
+        time: '01:40 PM - 02:30 PM',
+        roomNumber: 'AB-103',
+        subject: '22AIE498 - Project Phase I',
+        type: '',
+        faculty: 'Dr. R.Annamalai'
+    }
+];
+
+const Tuesday = [
+    {   
+        time: '08:25 AM - 09:15 AM', 
+        roomNumber: 'AB-103',
+        subject: '22AIE460 - Software Design Patterns [P.E.3]',
+        type: '',
+        faculty: 'Dr K Venkataraman, Ms M Rithani [Assist]'
+    },
+    {   
+        time: '09:15 AM - 10:05 AM',
+        roomNumber: 'AB-103',
+        subject: '22AIE401 - Reinforcement Learning',
+        type: '',
+        faculty: 'Dr. R.PrasannaKumar, Ms.G.Pallavi [Assist]'
+    },
+    {   
+        time: '10:05 AM - 10:55 AM',
+        roomNumber: 'AB-103',
+        subject: '19MEE347 - Total quality management[F.E.2]',
+        type: '',
+        faculty: 'Dr. Vignesh M'
+    },
+    {   
+        time: '11:10 AM - 12:00 PM', 
+        roomNumber: 'AB-103',
+        subject: '22AIE498 - Project Phase I',
+        type: '',
+        faculty: 'Dr. R.Annamalai'
+    },
+    {   
+        time: '01:40 PM - 02:30 PM', 
+        roomNumber: 'AB-103',
+        subject: '22AIE460 Lab 2 - Software Design Patterns Lab', // Assuming Lab for 22AIE460
+        type: '',
+        faculty: 'Dr K Venkataraman, Ms M Rithani [Assist]'
+    }
+];
+
+const Wednesday = [
+    {   
+        time: '08:25 AM - 09:15 AM',
+        roomNumber: 'AB-103',
+        subject: 'Prayer',
+        type: '',
+        faculty: ''
+    },
+    {   
+        time: '09:15 AM - 10:05 AM',
+        roomNumber: 'AB-103',
+        subject: '22AIE401 - Reinforcement Learning',
+        type: '',
+        faculty: 'Dr. R.PrasannaKumar, Ms.G.Pallavi [Assist]'
+    },
+    {   
+        time: '10:05 AM - 10:55 AM',
+        roomNumber: 'AB-103',
+        subject: '19MEE347 - Total quality management[F.E.2]',
+        type: '',
+        faculty: 'Dr. Vignesh M'
+    },
+    {   
+        time: '11:10 AM - 12:00 PM',
+        roomNumber: 'AB-103',
+        subject: 'Student Interaction with Program Chair Counselling',
+        type: '',
+        faculty: '' // No specific faculty mentioned for this
+    },
+    {   
+        time: '12:00 PM - 12:50 PM',
+        roomNumber: 'AB-103',
+        subject: 'Counselling Student Interaction with Chairperson',
+        type: '',
+        faculty: '' // No specific faculty mentioned for this
+    },
+    {   
+        time: '01:40 PM - 02:30 PM',
+        roomNumber: 'AB-103',
+        subject: '22AIE498 - Project Phase I',
+        type: '',
+        faculty: 'Dr. R.Annamalai'
+    },
+    {   
+        time: '02:30 PM - 03:20 PM',
+        roomNumber: 'AB-103',
+        subject: 'Remedial Classes',
+        type: '',
+        faculty: '' // No specific faculty mentioned for this
+    }
+];
+
+const Thursday = [
+    {   
+        time: '08:25 AM - 09:15 AM',
+        roomNumber: 'AB-103',
+        subject: 'Prayer',
+        type: '',
+        faculty: ''
+    },
+    {   
+        time: '09:15 AM - 10:05 AM',
+        roomNumber: 'AB-103',
+        subject: '22AIE460 - Software Design Patterns [P.E.3]',
+        type: '',
+        faculty: 'Dr K Venkataraman, Ms M Rithani [Assist]'
+    },
+    {   
+        time: '10:05 AM - 10:55 AM',
+        roomNumber: 'AB-103',
+        subject: 'Career Prep Class',
+        type: '',
+        faculty: '' // No specific faculty mentioned for this, but could be related to CIR/APT/VER/SSK
+    },
+    {   
+        time: '11:10 AM - 12:00 PM',
+        roomNumber: 'AB-103',
+        subject: 'Student Interaction with Program Chair Counselling',
+        type: '',
+        faculty: '' // No specific faculty mentioned for this
+    },
+    {   
+        time: '12:00 PM - 12:50 PM',
+        roomNumber: 'AB-103',
+        subject: 'Counselling Student Interaction with Chairperson',
+        type: '',
+        faculty: '' // No specific faculty mentioned for this
+    },
+    {   
+        time: '01:40 PM - 02:30 PM',
+        roomNumber: 'AB-103',
+        subject: '22AIE498 - Project Phase I',
+        type: '',
+        faculty: 'Dr. R.Annamalai'
+    },
+    {   
+        time: '02:30 PM - 03:20 PM',
+        roomNumber: 'AB-103',
+        subject: 'Remedial Classes',
+        type: '',
+        faculty: '' // No specific faculty mentioned for this
+    }
+];
+
+const Friday = [
+    {   
+        time: '08:25 AM - 09:15 AM', 
+        roomNumber: 'AB-103',
+        subject: '22AIE457 Lab 2 - Full Stack Development Lab', // Assuming Lab for 22AIE457
+        type: '',
+        faculty: 'Dr.G.BharathiMohan, Ms M Rithani [Assist]'
+    },
+    {   
+        time: '10:05 AM - 10:55 AM', 
+        roomNumber: 'AB-103',
+        subject: '19LAW300 - Indian Constitution',
+        type: '',
+        faculty: 'Dr. C. Sarathchandran'
+    },
+    {   
+        time: '11:10 AM - 12:00 PM',
+        roomNumber: 'AB-103',
+        subject: 'Library',
+        type: '',
+        faculty: 'AJ.Srikanth and Saptarshi chatterjee'
+    },
+    {   
+        time: '12:00 PM - 12:50 PM',
+        roomNumber: 'AB-103',
+        subject: 'APT',
+        type: '',
+        faculty: 'K.Sabariand EV. Kamal prasad'
+    },
+    {   
+        time: '01:40 PM - 02:30 PM',
+        roomNumber: 'AB-103',
+        subject: 'VER',
+        type: '',
+        faculty: 'VR.Chitra and B. Gayathri'
+    },
+    {   
+        time: '02:30 PM - 03:20 PM',
+        roomNumber: 'AB-103',
+        subject: 'SSK',
+        type: '',
+        faculty: 'AJ.Srikanth and Saptarshi chatterjee'
+    }
+];
+
+const Saturday = [
+    {   
+        time: 'Saturday',
+        roomNumber: 'No Class',
+        subject: 'No class Available',
+        type: '',
+        faculty: ''
+    }
+];
